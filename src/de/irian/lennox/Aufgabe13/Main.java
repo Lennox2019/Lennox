ce5d079b84950121cdfa7876b07452b0dfff8238
Lennox2019 <lennox_lee@web.de>
2019-10-29 13:47:56 +0100

    Lennox Aufgabe 10 f)

--- src/de/irian/lennox/Aufgabe13/Main.java
+++ src/de/irian/lennox/Aufgabe13/Main.java
@@ -1,10 +1,33 @@
 package de.irian.lennox.Aufgabe13;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Person klaus = new Person("klaus", 78, 1.75, 24);
-        double Bmi = klaus.gewicht / (klaus.groesse * klaus.groesse);
-        System.out.println("Bmi von " + klaus.name + " ist:" + Bmi);
+        String name;
+        float gewicht;
+        double groesse;
+        int alter;
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Die groesse ist: ");
+        groesse = scanner.nextDouble();
+
+        System.out.print("Das gewicht ist:");
+        gewicht=scanner.nextFloat();
+
+        System.out.print("Das alter ist:");
+        alter=scanner.nextInt();
+
+        System.out.print("Der name ist:");
+        name=scanner.next();
+
+
+
+        Person person = new Person(name, gewicht, groesse, alter);
+        double Bmi = person.gewicht / (person.groesse * person.groesse);
+        System.out.println("Bmi von " + person.name + " ist:" + Bmi);
 
 
 
--- src/de/irian/lennox/LennoxAufgabe3.java
+++ src/de/irian/lennox/LennoxAufgabe3.java
@@ -3,14 +3,15 @@ package de.irian.lennox;
 import java.util.Scanner;
 
 public class LennoxAufgabe3 {
-    public static void main(String[] args){
-        float gewicht  = 78;
-        float groesse = 0;
-        int mehrwertsteuer = 19;
-        Scanner scanner = new Scanner (System.in);
-        System.out.print("Der Nettopreis ist: ");
-        nettopreis = scanner.nextFloat();
-        brutopreis = nettopreis + (nettopreis / 100 * mehrwertsteuer);
-        System.out.print("Der Brutopreis ist: "+ brutopreis);
+    public static void main(String[] args) {
+        float gewicht = 78;
+        double groesse = 1.75;
+        int alter = 24;
+        double Bmi = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Der der Bmi ist: ");
+        Bmi = scanner.nextFloat();
+        Bmi = gewicht / (groesse * groesse);
+        System.out.print("Der Bmi ist: " + Bmi);
     }
 }
