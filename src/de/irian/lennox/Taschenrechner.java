454cc4556709bfe0888d865cdaf41b080dba8f97
Lennox2019 <lennox_lee@web.de>
2019-10-30 11:34:40 +0100

    Taschenrechner

--- src/de/irian/lennox/Taschenrechner.java
+++ src/de/irian/lennox/Taschenrechner.java
@@ -4,47 +4,48 @@ import java.util.Scanner;
 
 public class Taschenrechner {
     public static void main(String[] args) {
-        double zahl1= 0;
-        double zahl2= 0;
+        double zahl1 = 0;
+        double zahl2 = 0;
         Scanner scanner = new Scanner(System.in);
-        String eingabe;
+        double ergebnis = 0;
+        String eingabe = "";
         String plus = "Plus";
         String minus = "Minus";
         String mal = "Mal";
-        String geteilt="Geteilt";
+        String geteilt = "Geteilt";
 
-        System.out.println("Bitte geben sie die gewünschte Rechenoption an");
-        System.out.println("Plus steht für +, Minus steht für -, Mal steht für * und Geteielt steht für /");
-        System.out.println("Rechenoption:");
-        eingabe = scanner.next();
+        while (!eingabe.equals("beenden")) {
+            System.out.println("Bitte geben sie die gewünschte Rechenoption an");
+            System.out.println("Plus steht für +, Minus steht für -, Mal steht für * und Geteielt steht für /");
+            System.out.println("Für Beenden beenden eingeben");
+            System.out.println("Rechenoption:");
+            eingabe = scanner.next();
 
-        if (eingabe.equals(plus)) {
-            System.out.println("Erste Zahl bitte:");
-            zahl1 = scanner.nextDouble();
-            System.out.println("Zweite Zahl bitte:");
-            zahl2 = scanner.nextDouble();
-            System.out.println("Ergebnis:" + (zahl1 + zahl2));
-        }
-        if (eingabe.equals(minus)) {
-            System.out.println("Erste Zahl bitte:");
-            zahl1 = scanner.nextDouble();
-            System.out.println("Zweite Zahl bitte:");
-            zahl2 = scanner.nextDouble();
-            System.out.println("Ergebnis:" + (zahl1 - zahl2));
-        }
-        if (eingabe.equals(mal)) {
-            System.out.println("Erste Zahl bitte:");
-            zahl1 = scanner.nextDouble();
-            System.out.println("Zweite Zahl bitte:");
-            zahl2 = scanner.nextDouble();
-            System.out.println("Ergebnis:" + zahl1 * zahl2);
-        }
-        if (eingabe.equals(geteilt)) {
-            System.out.println("Erste Zahl bitte:");
-            zahl1 = scanner.nextDouble();
-            System.out.println("Zweite Zahl bitte:");
-            zahl2 = scanner.nextDouble();
-            System.out.println("Ergebnis:" + zahl1 / zahl2);
+            if (eingabe.equals("beenden")) {
+                System.out.println("Bye Bye");
+            }
+            else if (!eingabe.equals(plus) && !eingabe.equals(minus) && !eingabe.equals(mal) && !eingabe.equals(geteilt)) {
+                System.out.println("Leider geht die Rechenoption nicht.");
+            } else {
+                System.out.println("Erste Zahl bitte:");
+                zahl1 = scanner.nextDouble();
+                System.out.println("Zweite Zahl bitte:");
+                zahl2 = scanner.nextDouble();
+
+                if (eingabe.equals(plus)) {
+                    ergebnis = zahl1 + zahl2;
+                }
+                if (eingabe.equals(minus)) {
+                    ergebnis = zahl1 - zahl2;
+                }
+                if (eingabe.equals(mal)) {
+                    ergebnis = zahl1 * zahl2;
+                }
+                if (eingabe.equals(geteilt)) {
+                    ergebnis = zahl1 / zahl2;
+                }
+                System.out.println("Ergebnis:" + ergebnis);
+            }
         }
     }
 }
