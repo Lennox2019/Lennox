bb87c2a88508b2d5d776c60d2e4f513225897d7f
Lennox2019 <lennox_lee@web.de>
2019-11-12 11:15:05 +0100

    added Labels

--- src/main/java/de/irian/lennox/schiffeversenken/SchiffeversenkenFX.java
+++ src/main/java/de/irian/lennox/schiffeversenken/SchiffeversenkenFX.java
@@ -1,7 +1,10 @@
 package de.irian.lennox.schiffeversenken;
 import javafx.application.Application;
+import javafx.geometry.HPos;
 import javafx.geometry.Orientation;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
@@ -18,13 +21,20 @@ public class SchiffeversenkenFX extends Application {
         stage.setTitle("Schiffe versenken");
         GridPane mainGrid = new GridPane();
 
+        Label yourField = new Label("Dein Feld");
+        mainGrid.add(yourField, 0, 0);
+
+        Label enemyField = new Label("Gegner Feld");
+        mainGrid.add(enemyField, 60,0);
+
+
         SchiffeversenkenSpielFeld mySchiffeversenkenSpielFeld = new SchiffeversenkenSpielFeld(Color.BLUE);
-        mainGrid.add(mySchiffeversenkenSpielFeld,0,0);
+        mainGrid.add(mySchiffeversenkenSpielFeld,0,1);
 
         mainGrid.setHgap(1.5);
 
         SchiffeversenkenSpielFeld enemySchiffeversenkenSpielFeld = new SchiffeversenkenSpielFeld(Color.GOLD);
-        mainGrid.add(enemySchiffeversenkenSpielFeld, 60, 0);
+        mainGrid.add(enemySchiffeversenkenSpielFeld, 60, 1);
 
         stage.setScene(new Scene (mainGrid));
         stage.setResizable(true);
